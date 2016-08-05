cff5e54ec6b8d9b95effcad1ca2dd8bb0988517b
gingerBill <ginger.bill.22@gmail.com>
2016-08-05 22:10:05 +0100

    LLVM Binaries

---
+++ misc/debug.bat
@@ -0,0 +1,3 @@
+@echo off
+
+call devenv odin.sln
---
+++ misc/shell.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x64 1> NUL
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 1> NUL
+set _NO_DEBUG_HEAP=1
+
+set path=w:\Odin\misc;%path%
+
+cls
