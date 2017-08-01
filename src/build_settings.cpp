c454ede18452591ada54bc24428c97663ccd0df4
Ginger Bill <bill@gingerbill.org>
2017-08-01 17:30:26 +0100

    v0.6.1a

--- src/build_settings.cpp
+++ src/build_settings.cpp
@@ -283,7 +283,7 @@ String get_fullpath_core(gbAllocator a, String path) {
 }
 
 
-String const ODIN_VERSION = str_lit("0.6.1");
+String const ODIN_VERSION = str_lit("0.6.1a");
 
 void init_build_context(void) {
 	BuildContext *bc = &build_context;
