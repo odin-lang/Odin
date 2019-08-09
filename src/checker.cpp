cb215b131c114ff265ecf4289c319196a583321f
gingerBill <bill@gingerbill.org>
2019-08-09 20:31:11 +0100

    Fix bit_field comparison against nil #414

--- src/checker.cpp
+++ src/checker.cpp
@@ -1615,6 +1615,7 @@ void generate_minimum_dependency_set(Checker *c, Entity *start) {
 		str_lit("udivti3"),
 
 		str_lit("memory_compare"),
+		str_lit("memory_compare_zero"),
 	};
 	for (isize i = 0; i < gb_count_of(required_runtime_entities); i++) {
 		add_dependency_to_set(c, scope_lookup(c->info.runtime_package->scope, required_runtime_entities[i]));
