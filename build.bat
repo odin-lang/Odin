bd84d276dea4e9e841876ea52a53b810e61e1cc7
gingerBill <bill@gingerbill.org>
2019-12-01 18:18:03 +0000

    Fix IR string interning type

--- build.bat
+++ build.bat
@@ -41,7 +41,7 @@ del *.ilk > NUL 2> NUL
 
 cl %compiler_settings% "src\main.cpp" ^
 	/link %linker_settings% -OUT:%exe_name% ^
-	&& odin build examples/demo/demo.odin -keep-temp-files
+	&& odin run examples/demo/demo.odin
 
 del *.obj > NUL 2> NUL
 
