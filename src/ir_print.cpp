0739f83013b94510f283602b96a5336b5eb8050a
gingerBill <bill@gingerbill.org>
2020-03-13 08:52:23 +0000

    Apply byval to sret for System V ABI

--- src/ir_print.cpp
+++ src/ir_print.cpp
@@ -338,9 +338,13 @@ void ir_print_proc_type_without_pointer(irFileBuffer *f, irModule *m, Type *t) {
 	ir_write_string(f, str_lit(" ("));
 	if (t->Proc.return_by_pointer) {
 		ir_print_type(f, m, reduce_tuple_to_single_type(t->Proc.results));
-		// ir_fprintf(f, "* sret noalias ");
+		ir_fprintf(f, "* sret noalias ");
+		if (build_context.ODIN_OS == "darwin" ||
+		    build_context.ODIN_OS == "linux") {
+			ir_fprintf(f, "byval ");
+		}
 		// ir_write_string(f, str_lit("* noalias "));
-		ir_write_string(f, str_lit("*"));
+		// ir_write_string(f, str_lit("*"));
 		if (param_count > 0 || t->Proc.calling_convention == ProcCC_Odin)  {
 			ir_write_string(f, str_lit(", "));
 		}
@@ -2379,6 +2383,10 @@ void ir_print_proc(irFileBuffer *f, irModule *m, irProcedure *proc) {
 	if (proc_type->return_by_pointer) {
 		ir_print_type(f, m, reduce_tuple_to_single_type(proc_type->results));
 		ir_write_str_lit(f, "* sret noalias ");
+		if (build_context.ODIN_OS == "darwin" ||
+		    build_context.ODIN_OS == "linux") {
+			ir_fprintf(f, "byval ");
+		}
 		ir_write_str_lit(f, "%agg.result");
 		param_index += 1;
 	}
