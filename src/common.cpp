acefb2edbc886f6ae1cbc7cb494383511e4d4eea
Nakst <nakst@protonmail.com>
2021-03-14 20:37:32 +0000

    Similar to the update to gb.h

--- src/common.cpp
+++ src/common.cpp
@@ -116,7 +116,7 @@ GB_ALLOCATOR_PROC(heap_allocator_proc) {
 #elif defined(GB_SYSTEM_LINUX)
 	// TODO(bill): *nix version that's decent
 	case gbAllocation_Alloc: {
-		ptr = aligned_alloc(alignment, size);
+		ptr = aligned_alloc(alignment, (size + alignment - 1) & ~(alignment - 1));
 		// ptr = malloc(size+alignment);
 
 		if (flags & gbAllocatorFlag_ClearToZero) {
