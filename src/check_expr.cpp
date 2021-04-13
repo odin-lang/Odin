12296a0dcc440e0ac79059dd33aa44caebf7560a
gingerBill <bill@gingerbill.org>
2021-04-13 18:57:47 +0100

    Allow `intrinsics` entities to be exported from other packages if wanted

--- src/check_expr.cpp
+++ src/check_expr.cpp
@@ -3515,7 +3515,7 @@ Entity *check_selector(CheckerContext *c, Operand *operand, Ast *node, Type *typ
 				if (entity->kind == Entity_Builtin) {
 					// NOTE(bill): Builtin's are in the universal scope which is part of every scopes hierarchy
 					// This means that we should just ignore the found result through it
-					allow_builtin = entity->scope == import_scope;
+					allow_builtin = entity->scope == import_scope || entity->scope != builtin_pkg->scope;
 				} else if ((entity->scope->flags&ScopeFlag_Global) == ScopeFlag_Global && (import_scope->flags&ScopeFlag_Global) == 0) {
 					is_declared = false;
 				}
