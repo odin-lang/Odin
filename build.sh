34bcb1f54e31aa382a7585c595007aefeb485c0a
Ali Chraghi <63465728+AliChraghi@users.noreply.github.com>
2021-04-23 19:40:52 +0430

    Update build.sh

--- build.sh
+++ build.sh
@@ -27,4 +27,8 @@ elif [[ "$(uname)" == "FreeBSD" ]]; then
 	compiler="clang"
 fi
 
+if [[ "$2" == "install" ]]; then
+	sudo ln -sf $PWD/odin /usr/bin/odin
+fi
+
 ${compiler} src/main.cpp ${warnings_to_disable} ${libraries} ${other_args} -o odin && ./odin run examples/demo/demo.odin
