60db2f791b256d5de6808c6e2d936cc8bfbc5781
gingerBill <bill@gingerbill.org>
2021-06-26 22:47:12 +0100

    Fix semicolon insertion rule for `---`

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -1409,14 +1409,14 @@ void tokenizer_get_token(Tokenizer *t, Token *token, int repeat=0) {
 			if (t->curr_rune == '=') {
 				advance_to_next_rune(t);
 				token->kind = Token_SubEq;
-			} else if (t->curr_rune == '-' && peek_byte(t) == '-') {
-				advance_to_next_rune(t);
-				advance_to_next_rune(t);
-				token->kind = Token_Undef;
 			} else if (t->curr_rune == '-') {
+				insert_semicolon = true;
 				advance_to_next_rune(t);
 				token->kind = Token_Decrement;
-				insert_semicolon = true;
+				if (t->curr_rune == '-') {
+					advance_to_next_rune(t);
+					token->kind = Token_Undef;
+				}
 			} else if (t->curr_rune == '>') {
 				advance_to_next_rune(t);
 				token->kind = Token_ArrowRight;
