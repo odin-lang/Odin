40b7b9ecdf3aa3c749b1bb1b04ae0637e49e16c7
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2021-08-08 23:41:51 +0200

    big: Refactor exponents and such.

--- core/math/big/build.bat
+++ core/math/big/build.bat
@@ -1,10 +1,8 @@
 @echo off
-odin run . -vet
+:odin run . -vet
 : -o:size
-:odin build . -build-mode:shared -show-timings -o:minimal -no-bounds-check
-:odin build . -build-mode:shared -show-timings -o:size -no-bounds-check
-:odin build . -build-mode:shared -show-timings -o:size
-:odin build . -build-mode:shared -show-timings -o:speed -no-bounds-check
-:odin build . -build-mode:shared -show-timings -o:speed
-
-:python test.py
+:odin build . -build-mode:shared -show-timings -o:minimal -no-bounds-check && python test.py
+:odin build . -build-mode:shared -show-timings -o:size -no-bounds-check  && python test.py
+:odin build . -build-mode:shared -show-timings -o:size  && python test.py
+odin build . -build-mode:shared -show-timings -o:speed -no-bounds-check && python test.py
+:odin build . -build-mode:shared -show-timings -o:speed && python test.py
