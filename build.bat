e722af7f6126d821ec2847769b234ed18f61518c
gingerBill <bill@gingerbill.org>
2021-08-19 15:43:51 +0100

    Remove unneeded disabled warnings from build.bat

--- build.bat
+++ build.bat
@@ -46,10 +46,8 @@ if %release_mode% EQU 0 ( rem Debug
 
 set compiler_warnings= ^
 	-W4 -WX ^
-	-wd4100 -wd4101 -wd4127 -wd4146 -wd4189 ^
-	-wd4201 -wd4204 ^
-	-wd4456 -wd4457 -wd4480 ^
-	-wd4512
+	-wd4100 -wd4101 -wd4127 -wd4146 ^
+	-wd4456 -wd4457
 
 set compiler_includes=
 set libs= ^
