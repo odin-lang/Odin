c4ec459d289efd85071e64aea01fe81c258c0a4f
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2021-09-07 13:26:34 +0200

    bit: Force Python3 for CI.

--- core/math/big/tests/build.bat
+++ core/math/big/tests/build.bat
@@ -4,6 +4,6 @@ set TEST_ARGS=
 set OUT_NAME=test_library
 set COMMON=-build-mode:shared -show-timings -no-bounds-check -define:MATH_BIG_EXE=false -vet -strict-style
 set PATH_TO_ODIN==..\..\..\..\odin
-%PATH_TO_ODIN% build . %COMMON% -o:minimal -out:%OUT_NAME% && python test.py %TEST_ARGS%
-%PATH_TO_ODIN% build . %COMMON% -o:size    -out:%OUT_NAME% && python test.py %TEST_ARGS%
-%PATH_TO_ODIN% build . %COMMON% -o:speed   -out:%OUT_NAME% && python test.py %TEST_ARGS%
+%PATH_TO_ODIN% build . %COMMON% -o:minimal -out:%OUT_NAME% && python3 test.py %TEST_ARGS%
+%PATH_TO_ODIN% build . %COMMON% -o:size    -out:%OUT_NAME% && python3 test.py %TEST_ARGS%
+%PATH_TO_ODIN% build . %COMMON% -o:speed   -out:%OUT_NAME% && python3 test.py %TEST_ARGS%
