fcb23abce435c34edb4464b9c349c91aab50d1f8
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2021-09-07 19:57:08 +0200

    Tests: Integrate `strings` into CI.

--- tests/core/build.bat
+++ tests/core/build.bat
@@ -1,17 +1,25 @@
 @echo off
 set COMMON=-show-timings -no-bounds-check -vet -strict-style
 set PATH_TO_ODIN==..\..\odin
-:python3 download_assets.py
+python3 download_assets.py
 echo ---
 echo Running core:image tests
 echo ---
 %PATH_TO_ODIN% test image    %COMMON%
 del image.exe
+
 echo ---
 echo Running core:compress tests
 echo ---
 %PATH_TO_ODIN% test compress %COMMON%
 del compress.exe
+
+echo ---
+echo Running core:strings tests
+echo ---
+%PATH_TO_ODIN% test strings %COMMON%
+del strings.exe
+
 
 rem math/big tests
 set TEST_ARGS=-fast-tests
