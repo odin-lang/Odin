4da5ed63b544ff0f53be13ede12ab29c48851453
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2021-09-07 19:47:37 +0200

    Move math/big tests under `tests/`.

--- tests/core/build.bat
+++ tests/core/build.bat
@@ -1,6 +1,27 @@
 @echo off
 set COMMON=-show-timings -no-bounds-check -vet -strict-style
 set PATH_TO_ODIN==..\..\odin
-python3 download_assets.py
+:python3 download_assets.py
+echo ---
+echo Running core:image tests
+echo ---
 %PATH_TO_ODIN% test image    %COMMON%
+del image.exe
+echo ---
+echo Running core:compress tests
+echo ---
 %PATH_TO_ODIN% test compress %COMMON%
+del compress.exe
+
+rem math/big tests
+set TEST_ARGS=-fast-tests
+set TEST_ARGS=
+set OUT_NAME=math_big_test_library
+set COMMON=-build-mode:shared -show-timings -no-bounds-check -define:MATH_BIG_EXE=false -vet -strict-style
+echo ---
+echo Running core:math/big tests
+echo ---
+
+%PATH_TO_ODIN% build math/big %COMMON% -o:speed -out:%OUT_NAME%
+python3 math/big/test.py %TEST_ARGS%
+del %OUT_NAME%.*
