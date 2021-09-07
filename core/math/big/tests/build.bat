008048e199bdbf6d84fad756a774d4e5e2420edc
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2021-09-07 12:06:58 +0200

    big: Move tests into their own package.

---
+++ core/math/big/tests/build.bat
@@ -0,0 +1,8 @@
+@echo off
+set TEST_ARGS=-fast-tests
+set TEST_ARGS=
+set OUT_NAME=test_library
+set COMMON=-build-mode:shared -show-timings -no-bounds-check -define:MATH_BIG_EXE=false -vet -strict-style
+:odin build . %COMMON% -o:minimal -out:%OUT_NAME% && python test.py %TEST_ARGS%
+odin build . %COMMON% -o:size    -out:%OUT_NAME% && python test.py %TEST_ARGS%
+:odin build . %COMMON% -o:speed   -out:%OUT_NAME% && python test.py %TEST_ARGS%
