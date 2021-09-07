86cfb2ea828c6904a54938281cb6fde659b73f39
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2021-09-07 13:22:44 +0200

    big CI: Set relative path to Odin.

--- core/math/big/tests/build.bat
+++ core/math/big/tests/build.bat
@@ -3,6 +3,7 @@ set TEST_ARGS=-fast-tests
 set TEST_ARGS=
 set OUT_NAME=test_library
 set COMMON=-build-mode:shared -show-timings -no-bounds-check -define:MATH_BIG_EXE=false -vet -strict-style
-:odin build . %COMMON% -o:minimal -out:%OUT_NAME% && python test.py %TEST_ARGS%
-:odin build . %COMMON% -o:size    -out:%OUT_NAME% && python test.py %TEST_ARGS%
-odin build . %COMMON% -o:speed   -out:%OUT_NAME% && python test.py %TEST_ARGS%
+set PATH_TO_ODIN==..\..\..\..\odin
+%PATH_TO_ODIN% build . %COMMON% -o:minimal -out:%OUT_NAME% && python test.py %TEST_ARGS%
+%PATH_TO_ODIN% build . %COMMON% -o:size    -out:%OUT_NAME% && python test.py %TEST_ARGS%
+%PATH_TO_ODIN% build . %COMMON% -o:speed   -out:%OUT_NAME% && python test.py %TEST_ARGS%
