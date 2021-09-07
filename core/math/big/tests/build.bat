c74f091df265d58f9838cdfc24676d56d56b4af4
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2021-09-07 13:17:47 +0200

    big: Run tests under CI.

--- core/math/big/tests/build.bat
+++ core/math/big/tests/build.bat
@@ -4,5 +4,5 @@ set TEST_ARGS=
 set OUT_NAME=test_library
 set COMMON=-build-mode:shared -show-timings -no-bounds-check -define:MATH_BIG_EXE=false -vet -strict-style
 :odin build . %COMMON% -o:minimal -out:%OUT_NAME% && python test.py %TEST_ARGS%
-odin build . %COMMON% -o:size    -out:%OUT_NAME% && python test.py %TEST_ARGS%
-:odin build . %COMMON% -o:speed   -out:%OUT_NAME% && python test.py %TEST_ARGS%
+:odin build . %COMMON% -o:size    -out:%OUT_NAME% && python test.py %TEST_ARGS%
+odin build . %COMMON% -o:speed   -out:%OUT_NAME% && python test.py %TEST_ARGS%
