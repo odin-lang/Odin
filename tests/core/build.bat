0fe7cdf4ac4eac98ddaa65cefcaceaf10a279335
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2021-09-07 20:24:02 +0200

    Separate math/big test.

--- tests/core/build.bat
+++ tests/core/build.bat
@@ -19,17 +19,3 @@ echo Running core:strings tests
 echo ---
 %PATH_TO_ODIN% test strings %COMMON%
 del strings.exe
-
-
-rem math/big tests
-set TEST_ARGS=-fast-tests
-set TEST_ARGS=
-set OUT_NAME=math_big_test_library
-set COMMON=-build-mode:shared -show-timings -no-bounds-check -define:MATH_BIG_EXE=false -vet -strict-style
-echo ---
-echo Running core:math/big tests
-echo ---
-
-%PATH_TO_ODIN% build math/big %COMMON% -o:speed -out:%OUT_NAME%
-python3 math/big/test.py %TEST_ARGS%
-del %OUT_NAME%.*
---
+++ tests/core/math/big/build.bat
@@ -0,0 +1,13 @@
+@echo off
+rem math/big tests
+set PATH_TO_ODIN==..\..\..\..\odin
+set TEST_ARGS=-fast-tests
+set TEST_ARGS=
+set OUT_NAME=math_big_test_library
+set COMMON=-build-mode:shared -show-timings -no-bounds-check -define:MATH_BIG_EXE=false -vet -strict-style
+echo ---
+echo Running core:math/big tests
+echo ---
+
+%PATH_TO_ODIN% build . %COMMON% -o:speed -out:%OUT_NAME%
+python3 test.py %TEST_ARGS%
