61ec6f2458a0a5f11948fd7db8f63593b10e12e8
gingerBill <bill@gingerbill.org>
2021-11-01 16:39:02 +0000

    Correct debug info for `matrix` type

--- src/llvm_backend_debug.cpp
+++ src/llvm_backend_debug.cpp
@@ -547,6 +547,7 @@ LLVMMetadataRef lb_debug_type(lbModule *m, Type *type) {
 		case Type_SimdVector:
 		case Type_RelativePointer:
 		case Type_RelativeSlice:
+		case Type_Matrix:
 			{
 				LLVMMetadataRef debug_bt = lb_debug_type(m, bt);
 				LLVMMetadataRef final_decl = LLVMDIBuilderCreateTypedef(m->debug_builder, debug_bt, name_text, name_len, file, line, scope, align_in_bits);
