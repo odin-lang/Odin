76054dddb7042863aa2575f2670095806a8567e0
gingerBill <bill@gingerbill.org>
2021-11-09 22:11:18 +0000

    Revert build.bat

--- build.bat
+++ build.bat
@@ -69,50 +69,13 @@ set linker_settings=%libs% %linker_flags%
 del *.pdb > NUL 2> NUL
 del *.ilk > NUL 2> NUL
 
-rem cl %compiler_settings% "src\main.cpp" "src\libtommath.cpp" /link %linker_settings% -OUT:%exe_name%
-rem if %errorlevel% neq 0 goto end_of_build
+cl %compiler_settings% "src\main.cpp" "src\libtommath.cpp" /link %linker_settings% -OUT:%exe_name%
+if %errorlevel% neq 0 goto end_of_build
 
-odin run examples/bug
-rem odin build examples/demo -use-separate-modules
-rem odin run examples/demo -o:minimal
-rem odin run examples/demo -o:size
-rem odin run examples/demo -o:speed -keep-temp-files
-rem odin run examples/demo -o:speed -keep-temp-files
+call build_vendor.bat
+if %errorlevel% neq 0 goto end_of_build
 
-rem set small_hellope_flags=-strict-style -no-bounds-check -default-to-nil-allocator -disable-assert -no-crt -o:size 
-rem set small_hellope_flags=-strict-style -no-bounds-check -default-to-nil-allocator -disable-assert -o:size 
-
-rem odin build examples/small_hellope %small_hellope_flags% -build-mode:asm
-rem odin run examples/small_hellope %small_hellope_flags% -keep-temp-files
-rem FOR /F "usebackq" %%A IN ('small_hellope.exe') DO echo %%~zA
-
-rem cl /nologo examples\small_hellope\small_hellope.c /O1 /link Kernel32.lib -out:small_hellope_c.exe /NODEFAULTLIB /entry:mainCRTStartup /subsystem:console
-rem small_hellope_c.exe
-rem FOR /F "usebackq" %%A IN ('small_hellope_c.exe') DO echo %%~zA
-
-
-rem odin run examples/demo
-rem odin run examples/sdl2 -vet
-rem odin check examples/all -vet
-rem odin run examples/bug
-
-rem odin check examples/wasm -strict-style -target:wasi_wasm32 
-rem odin check examples/demo -strict-style
-rem odin build examples/wasm -strict-style -target:wasi_wasm32 -keep-temp-files
-rem "C:\Program Files\WAVM\bin\wavm.exe" disassemble wasm.wasm > wasm.wast
-rem "C:\Program Files\WAVM\bin\wavm.exe" run --abi=wasi --function=weird_add wasm.wasm 2 3
-
-rem wasmer inspect wasm.wasm
-rem wasmer run wasm.wasm
-
-rem odin run examples/demo -strict-style
-
-rem call build_vendor.bat
-rem if %errorlevel% neq 0 goto end_of_build
-
-rem if %release_mode% EQU 0 odin check examples/bug -strict-style
-
-rem odin run examples/demo -strict-style
+if %release_mode% EQU 0 odin run examples/demo
 
 del *.obj > NUL 2> NUL
 
