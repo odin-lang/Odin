516065d7c22c19aa953edf1d4ab1537f07742dec
Henry Dooley <henry.dooley@gmail.com>
2021-12-25 16:27:52 -0600

    factor out alloca generation into a helper

--- src/llvm_backend_const.cpp
+++ src/llvm_backend_const.cpp
@@ -413,9 +413,9 @@ lbValue lb_const_value(lbModule *m, Type *type, ExactValue value, bool allow_loc
 				LLVMPositionBuilderAtEnd(p->builder, p->decl_block->block);
 
 				LLVMTypeRef llvm_type = lb_type(m, t);
-				array_data = LLVMBuildAlloca(p->builder, llvm_type, "");
-				LLVMSetAlignment(array_data, 16); // TODO(bill): Make this configurable
-				LLVMPositionBuilderAtEnd(p->builder, p->curr_block->block);
+
+				array_data = llvm_alloca(p, llvm_type, 16);
+
 				LLVMBuildStore(p->builder, backing_array.value, array_data);
 
 				{
