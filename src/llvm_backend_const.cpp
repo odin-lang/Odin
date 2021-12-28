a7073e35133835cdd72bee3c729faadd4caceae0
Henry Dooley <henry.dooley@gmail.com>
2021-12-28 10:50:34 -0600

    review feedback

--- src/llvm_backend_const.cpp
+++ src/llvm_backend_const.cpp
@@ -410,8 +410,6 @@ lbValue lb_const_value(lbModule *m, Type *type, ExactValue value, bool allow_loc
 				// NOTE(bill, 2020-06-08): This is a bit of a hack but a "constant" slice needs
 				// its backing data on the stack
 				lbProcedure *p = m->curr_procedure;
-				LLVMPositionBuilderAtEnd(p->builder, p->decl_block->block);
-
 				LLVMTypeRef llvm_type = lb_type(m, t);
 
 				array_data = llvm_alloca(p, llvm_type, 16);
