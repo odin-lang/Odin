9b1d538271b9f6e986d52edcbfd57b7d634165b1
gingerBill <bill@gingerbill.org>
2022-09-07 11:17:33 +0100

    Add inline debug generation for assembly target flag (#2028)

--- src/llvm_backend.cpp
+++ src/llvm_backend.cpp
@@ -1420,7 +1420,7 @@ void lb_generate_code(lbGenerator *gen) {
 				}
 			}
 
-			LLVMBool split_debug_inlining = false;
+			LLVMBool split_debug_inlining = build_context.build_mode == BuildMode_Assembly;
 			LLVMBool debug_info_for_profiling = false;
 
 			m->debug_compile_unit = LLVMDIBuilderCreateCompileUnit(m->debug_builder, LLVMDWARFSourceLanguageC99,
