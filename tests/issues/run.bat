bde2f85a7e86978525f27a8ac3df23bf73b687fd
Lucas Perlind <perlindluca@gmail.com>
2022-10-08 11:44:08 +1100

    Fix for issue 2113: Add debug information for soa pointers
    Additionally:
    * Add a compilation test case for the issue
    * Run issue CI tests on more platforms
    * Build tests in the build folder

--- tests/issues/run.bat
+++ tests/issues/run.bat
@@ -1,15 +1,18 @@
 @echo off
 
 if not exist "build\" mkdir build
+pushd build
 
-set COMMON=-collection:tests=..
+set COMMON=-collection:tests=..\..
 
 @echo on
 
-..\..\odin test test_issue_829.odin %COMMON% -file
-..\..\odin test test_issue_1592.odin %COMMON% -file
-..\..\odin test test_issue_2087.odin %COMMON% -file
+..\..\..\odin test ..\test_issue_829.odin %COMMON% -file
+..\..\..\odin test ..\test_issue_1592.odin %COMMON% -file
+..\..\..\odin test ..\test_issue_2087.odin %COMMON% -file
+..\..\..\odin run ..\test_issue_2113.odin %COMMON% -file -debug
 
 @echo off
 
+popd
 rmdir /S /Q build
