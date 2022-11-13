9b88a38e54818daf6fda4e2dda4ab4aa6c6dbd5d
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2022-11-13 16:32:24 +0100

    map tests for Linux and Mac

--- tests/internal/build.bat
+++ tests/internal/build.bat
@@ -1,3 +1,4 @@
 @echo off
 set PATH_TO_ODIN==..\..\odin
-%PATH_TO_ODIN% run test_map.odin -file -vet -strict-style -o:minimal -define:SEED=42
+%PATH_TO_ODIN% run test_map.odin -file -vet -strict-style -o:minimal
+rem -define:SEED=42
