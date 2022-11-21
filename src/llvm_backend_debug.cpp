27d56d0da4c32fae3b52d8dcff65f111119b8d45
gingerBill <bill@gingerbill.org>
2022-11-21 10:25:34 +0000

    Fix #2125

--- src/llvm_backend_debug.cpp
+++ src/llvm_backend_debug.cpp
@@ -294,6 +294,7 @@ LLVMMetadataRef lb_debug_type_internal(lbModule *m, Type *type) {
 		GB_PANIC("Type_Named should be handled in lb_debug_type separately");
 
 	case Type_SoaPointer:
+		return LLVMDIBuilderCreatePointerType(m->debug_builder, lb_debug_type(m, type->SoaPointer.elem), word_bits, word_bits, 0, nullptr, 0);
 	case Type_Pointer:
 		return LLVMDIBuilderCreatePointerType(m->debug_builder, lb_debug_type(m, type->Pointer.elem), word_bits, word_bits, 0, nullptr, 0);
 	case Type_MultiPointer:
