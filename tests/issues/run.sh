ada42aa18441584d503eaa88f08a895be4284015
J.C. Moyer <jcmoyer32@gmail.com>
2023-05-09 12:37:12 -0400

    Add test for issue #2056

--- tests/issues/run.sh
+++ tests/issues/run.sh
@@ -10,6 +10,7 @@ set -x
 
 $ODIN test ../test_issue_829.odin  $COMMON -file
 $ODIN test ../test_issue_1592.odin $COMMON -file
+$ODIN test ../test_issue_2056.odin $COMMON -file
 $ODIN test ../test_issue_2087.odin $COMMON -file
 $ODIN build ../test_issue_2113.odin $COMMON -file -debug
 
