49058620f090062e1e1a7b3158b22d17fe3e3dfa
Laytan Laats <laytanlaats@hotmail.com>
2023-07-02 21:25:48 +0200

    fix spacing

--- src/main.cpp
+++ src/main.cpp
@@ -2550,20 +2550,20 @@ gb_internal int strip_semicolons(Parser *parser) {
 gb_internal void init_terminal(void) {
 	build_context.has_ansi_terminal_colours = false;
 
-    gbAllocator a = heap_allocator();
+	gbAllocator a = heap_allocator();
 
-    char const *no_color = gb_get_env("NO_COLOR", a);
-    defer (gb_free(a, cast(void *)no_color));
-    if (no_color != nullptr) {
-        return;
-    }
+	char const *no_color = gb_get_env("NO_COLOR", a);
+	defer (gb_free(a, cast(void *)no_color));
+	if (no_color != nullptr) {
+		return;
+	}
 
-    char const *force_color = gb_get_env("FORCE_COLOR", a);
-    defer (gb_free(a, cast(void *)force_color));
-    if (force_color != nullptr) {
-        build_context.has_ansi_terminal_colours = true;
-        return;
-    }
+	char const *force_color = gb_get_env("FORCE_COLOR", a);
+	defer (gb_free(a, cast(void *)force_color));
+	if (force_color != nullptr) {
+		build_context.has_ansi_terminal_colours = true;
+		return;
+	}
 
 #if defined(GB_SYSTEM_WINDOWS)
 	HANDLE hnd = GetStdHandle(STD_ERROR_HANDLE);
@@ -2575,12 +2575,12 @@ gb_internal void init_terminal(void) {
 		}
 	}
 #elif defined(GB_SYSTEM_OSX) || defined(GB_SYSTEM_UNIX)
-    char const *term_ = gb_get_env("TERM", a);
-    defer (gb_free(a, cast(void *)term_));
-    String term = make_string_c(term_);
-    if (!str_eq(term, str_lit("dumb")) && isatty(STDERR_FILENO)) {
-        build_context.has_ansi_terminal_colours = true;
-    }
+	char const *term_ = gb_get_env("TERM", a);
+	defer (gb_free(a, cast(void *)term_));
+	String term = make_string_c(term_);
+	if (!str_eq(term, str_lit("dumb")) && isatty(STDERR_FILENO)) {
+		build_context.has_ansi_terminal_colours = true;
+	}
 #endif
 
 	if (!build_context.has_ansi_terminal_colours) {
