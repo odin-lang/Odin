aa38889704f3518878aeb82cb03d63c8b592ac0e
gingerBill <bill@gingerbill.org>
2023-07-07 23:23:47 +0100

    Fix issue with pointer casting internal llvm intrinsics

--- src/llvm_backend_const.cpp
+++ src/llvm_backend_const.cpp
@@ -483,7 +483,10 @@ gb_internal lbValue lb_const_value(lbModule *m, Type *type, ExactValue value, bo
 		GB_ASSERT(res.value != nullptr);
 		GB_ASSERT(LLVMGetValueKind(res.value) == LLVMFunctionValueKind);
 
-		res.value = LLVMConstPointerCast(res.value, lb_type(m, res.type));
+		if (LLVMGetIntrinsicID(res.value) == 0) {
+			// NOTE(bill): do not cast intrinsics as they are not really procedures that can be casted
+			res.value = LLVMConstPointerCast(res.value, lb_type(m, res.type));
+		}
 		return res;
 	}
 
