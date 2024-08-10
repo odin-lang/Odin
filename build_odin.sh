d98f84824784496b38f8b22d24423ba4243e8847
Laytan Laats <laytanlaats@hotmail.com>
2024-08-10 19:07:30 +0200

    fix debug compiler build message not ending output with a newline

--- build_odin.sh
+++ build_odin.sh
@@ -154,7 +154,7 @@ if [ $# -eq 0 ]; then
 	run_demo
 
 	: ${PROGRAM:=$0}
-	printf "\nDebug compiler built. Note: run \"$PROGRAM release\" or \"$PROGRAM release-native\" if you want a faster, release mode compiler."
+	printf "\nDebug compiler built. Note: run \"$PROGRAM release\" or \"$PROGRAM release-native\" if you want a faster, release mode compiler.\n"
 elif [ $# -eq 1 ]; then
 	case $1 in
 	report)
