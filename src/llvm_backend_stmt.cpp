722b638e2cbefcc1735256ef712cf2b28078a7fb
Laytan <laytanlaats@hotmail.com>
2024-08-31 19:40:05 +0200

    "fix" #4169, looks like llvm 14 bug

--- src/llvm_backend_stmt.cpp
+++ src/llvm_backend_stmt.cpp
@@ -2199,8 +2199,7 @@ gb_internal void lb_build_if_stmt(lbProcedure *p, Ast *node) {
 	// and `LLVMConstIntGetZExtValue()` calls below will be valid and `LLVMInstructionEraseFromParent()`
 	// will target the correct (& only) branch statement
 
-
-	if (cond.value && LLVMIsConstant(cond.value)) {
+	if (cond.value && LLVMIsAConstantInt(cond.value)) {
 		// NOTE(bill): Do a compile time short circuit for when the condition is constantly known.
 		// This done manually rather than relying on the SSA passes because sometimes the SSA passes
 		// miss some even if they are constantly known, especially with few optimization passes.
