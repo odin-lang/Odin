140c902eff2569e5f1491fc12649c6718a6ba600
Laytan Laats <laytanlaats@hotmail.com>
2025-03-12 18:45:14 +0100

    vendor/libc: a bunch of additions
    All these additions are to allow Dear ImGui to be compiled natively.

--- vendor/libc/include/math.h
+++ vendor/libc/include/math.h
@@ -1,21 +1,66 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma once
+
 #include <stdbool.h>
+
+#define INFINITY (1.0 / 0.0)
+#define NAN      (0.0 / 0.0)
 
 float sqrtf(float);
 float cosf(float);
 float sinf(float);
 float atan2f(float, float);
-bool isnan(float);
-bool isinf(float);
+
+float floorf(float x);
 double floor(double x);
+float ceilf(float x);
 double ceil(double x);
 double sqrt(double x);
+float powf(float x, float y);
 double pow(double x, double y);
+float fmodf(float x, float y);
 double fmod(double x, double y);
 double cos(double x);
+float acosf(float x);
 double acos(double x);
+float fabsf(float x);
 double fabs(double x);
 int abs(int);
 double ldexp(double, int);
 double exp(double);
+float logf(float);
 double log(double);
 double sin(double);
+double trunc(double);
+double log2(double);
+double log10(double);
+double asin(double);
+double atan(double);
+double tan(double);
+double atan2(double, double);
+double modf(double, double*);
+
+bool __isnanf(float);
+bool __isnand(double);
+#define isnan(x)                                           \
+    ( sizeof(x) == sizeof(float)  ? __isnanf((float)(x))   \
+    :                             : __isnand((double)(x)))
+
+bool __isinff(float);
+bool __isinfd(double);
+#define isinf(x)                                           \
+    ( sizeof(x) == sizeof(float)  ? __isinff((float)(x))   \
+    :                             : __isinfd((double)(x)))
+
+bool __isfinitef(float);
+bool __isfinited(double);
+#define isfinite(x)                                           \
+    ( sizeof(x) == sizeof(float)  ? __isfinitef((float)(x))   \
+    :                             : __isfinited((double)(x)))
+
+#ifdef __cplusplus
+}
+#endif
