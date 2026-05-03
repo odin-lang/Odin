517349acd33d411f6626e832ddc9dfe495aabf29
Krzesimir Nowak <qdlacz@gmail.com>
2026-05-03 14:36:25 +0200

    Make typed literal values on left-hand side more consistent
    There are two forms of literal values:
    
    - naked (`{ … }`)
    - typed (`type { … }` or `pkg.type { … }`)
    
    Left-hand side expressions beginning with a naked literal value are
    not accepted by the parser and this is controlled by parse_operand
    with its lhs parameter.
    
    Left-hand side expressions beginning with a typed literal value are
    only accepted by the parser if they come in the form of `pkg.type { … }`.
    This is because `pkg` is an lhs operand, `.type` is an lhs atom expr
    and `{` becomes an rhs atom expr. Braces are accepted in rhs atom
    expressions.
    
    I'd argue that typed literal values of both forms should be accepted
    in such expressions even if their usefulness is rather limited. This
    makes language a bit more consistent and the parse_atom_expr a bit
    simpler.
    
    And in contexts where typed literal values should not be accepted,
    setting `expr_level` to -1 should be used (like in
    parse_type_or_ident).
    
    Demo code:
    
    In "pkg" directory:
    
    ```odin
    package pkg
    
    Some_Type :: struct {
    	ptr : ^int
    }
    ```
    
    In parent directory:
    
    ```odin
    package main
    import p "./pkg"
    
    T :: p.Some_Type
    
    main :: proc() {
    	i : int
    	p.Some_Type{ ptr = &i }.ptr^ = 42
    	assert(i == 42)
    	// Does not compile.
    	// Error is "Syntax Error: Expected ';', got {".
    	// This PR fixes it.
    	T{ ptr = &i }.ptr^ = 13
    	assert(i == 13)
    }
    ```

--- src/parser.cpp
+++ src/parser.cpp
@@ -3314,7 +3314,7 @@ gb_internal void parse_check_or_return(Ast *operand, char const *msg) {
 	}
 }
 
-gb_internal Ast *parse_atom_expr(AstFile *f, Ast *operand, bool lhs) {
+gb_internal Ast *parse_atom_expr(AstFile *f, Ast *operand) {
 	if (operand == nullptr) {
 		if (f->allow_type) return nullptr;
 		Token begin = f->curr_token;
@@ -3473,7 +3473,7 @@ gb_internal Ast *parse_atom_expr(AstFile *f, Ast *operand, bool lhs) {
 			break;
 
 		case Token_OpenBrace:
-			if (!lhs && is_literal_type(operand) && f->expr_level >= 0) {
+			if (is_literal_type(operand) && f->expr_level >= 0) {
 				operand = parse_literal_value(f, operand);
 			} else {
 				loop = false;
@@ -3482,11 +3482,9 @@ gb_internal Ast *parse_atom_expr(AstFile *f, Ast *operand, bool lhs) {
 
 		case Token_Increment:
 		case Token_Decrement:
-			if (!lhs) {
+			{
 				Token token = advance_token(f);
 				syntax_error(token, "Postfix '%.*s' operator is not supported", LIT(token.string));
-			} else {
-				loop = false;
 			}
 			break;
 
@@ -3494,8 +3492,6 @@ gb_internal Ast *parse_atom_expr(AstFile *f, Ast *operand, bool lhs) {
 			loop = false;
 			break;
 		}
-
-		lhs = false; // NOTE(bill): 'tis not lhs anymore
 	}
 
 	return operand;
@@ -3551,7 +3547,7 @@ gb_internal Ast *parse_unary_expr(AstFile *f, bool lhs) {
 	}
 	}
 
-	return parse_atom_expr(f, parse_operand(f, lhs), lhs);
+	return parse_atom_expr(f, parse_operand(f, lhs));
 }
 
 gb_internal bool is_ast_range(Ast *expr) {
@@ -4655,7 +4651,7 @@ gb_internal Ast *parse_type_or_ident(AstFile *f) {
 
 	bool lhs = true;
 	Ast *operand = parse_operand(f, lhs);
-	Ast *type = parse_atom_expr(f, operand, lhs);
+	Ast *type = parse_atom_expr(f, operand);
 	return type;
 }
 
