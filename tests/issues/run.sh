a97f6b47b60b049b6fc3bfc9d5c957c36d52b8ff
Jeroen van Rijn <Kelimion@users.noreply.github.com>
2026-08-04 15:23:07 +0200

    Don't use `-microarch:native` on `odin check`

--- tests/issues/run.sh
+++ tests/issues/run.sh
@@ -5,6 +5,8 @@ mkdir -p build
 pushd build
 ODIN=../../../odin
 COMMON="-define:ODIN_TEST_FANCY=false -file -vet -strict-style -ignore-unused-defineables -microarch:native"
+COMMON_CHECK="-define:ODIN_TEST_FANCY=false -file -vet -strict-style -ignore-unused-defineables"
+
 
 set -x
 
@@ -73,14 +75,14 @@ else
 	echo "SUCCESSFUL 0/1"
 	exit 1
 fi
-$ODIN check ../test_issue_6484.odin -no-entry-point $COMMON
-if [[ $($ODIN check ../test_issue_6874.odin $COMMON 2>&1 >/dev/null | grep -c "Error:") -eq 1 ]] ; then
+$ODIN check ../test_issue_6484.odin -no-entry-point $COMMON_CHECK
+if [[ $($ODIN check ../test_issue_6874.odin $COMMON_CHECK 2>&1 >/dev/null | grep -c "Error:") -eq 1 ]] ; then
 	echo "SUCCESSFUL 1/1"
 else
 	echo "SUCCESSFUL 0/1"
 	exit 1
 fi
-$ODIN check ../test_issue_6979.odin -no-entry-point $COMMON
+$ODIN check ../test_issue_6979.odin -no-entry-point $COMMON_CHECK
 $ODIN build ../test_issue_7037.odin $COMMON -o:none
 
 if [[ $($ODIN build ../test_issue_7108.odin $COMMON 2>&1 >/dev/null | grep -c "Error:") -eq 2 ]] ; then
