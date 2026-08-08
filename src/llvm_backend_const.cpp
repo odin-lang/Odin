92ec71fa757ebafbfecfe7795508bdfad3b50d20
korvahkh <92224397+korvahkh@users.noreply.github.com>
2026-08-08 11:01:11 -0500

    Prevent underflow of union padding

--- src/llvm_backend_const.cpp
+++ src/llvm_backend_const.cpp
@@ -886,6 +886,8 @@ gb_internal lbValue lb_const_value(lbModule *m, Type *type, ExactValue value, lb
 				return res;
 			}
 		} else {
+			// NOTE(korvahkh): forces calculation of variant_block_size
+			type_size_of(bt);
 			i64 block_size = bt->Union.variant_block_size;
 
 			lbValue cv = lb_const_value(m, value_type, value, cc);
