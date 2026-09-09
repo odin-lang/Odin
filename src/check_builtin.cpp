9fc28251f986a16451080b2e8a7ca4fce67b61d1
LouisTruch <louis.truchelut@gmail.com>
2026-09-09 12:12:59 +0200

    intrinsics.type_elem_type() added case for Type_FixedCapacityDynamicArray

--- src/check_builtin.cpp
+++ src/check_builtin.cpp
@@ -6948,12 +6948,13 @@ gb_internal bool check_builtin_procedure(CheckerContext *c, Operand *operand, As
 				case Basic_quaternion256: operand->type = t_f64; break;
 				}
 				break;
-			case Type_Pointer:         operand->type = bt->Pointer.elem;         break;
-			case Type_Array:           operand->type = bt->Array.elem;           break;
-			case Type_EnumeratedArray: operand->type = bt->EnumeratedArray.elem; break;
-			case Type_Slice:           operand->type = bt->Slice.elem;           break;
-			case Type_DynamicArray:    operand->type = bt->DynamicArray.elem;    break;
-			case Type_SimdVector:      operand->type = bt->SimdVector.elem;      break;
+			case Type_Pointer:                   operand->type = bt->Pointer.elem;                   break;
+			case Type_Array:                     operand->type = bt->Array.elem;                     break;
+			case Type_EnumeratedArray:           operand->type = bt->EnumeratedArray.elem;           break;
+			case Type_Slice:                     operand->type = bt->Slice.elem;                     break;
+			case Type_DynamicArray:              operand->type = bt->DynamicArray.elem;              break;
+			case Type_FixedCapacityDynamicArray: operand->type = bt->FixedCapacityDynamicArray.elem; break;
+			case Type_SimdVector:                operand->type = bt->SimdVector.elem;                break;
 			}
 		}
 		operand->mode = Addressing_Type;
