a2ba92f501f7b74ea2d17c72f0e026a45790a731
Louis Novy <101842021+louisnovy@users.noreply.github.com>
2026-09-25 00:03:54 -0400

    check_type: don't cache polymorphic record specializations

--- src/check_type.cpp
+++ src/check_type.cpp
@@ -349,6 +349,12 @@ gb_internal void add_polymorphic_record_entity(CheckerContext *ctx, Ast *node, T
 	// TODO(bill): Is this even correct? Or should the metadata be copied?
 	e->TypeName.objc_metadata = original_type->Named.type_name->TypeName.objc_metadata;
 
+	// NOTE: `find_polymorphic_record_entity` never matches a polymorphic specialization.
+	// Adding one here would just grow gen_types by one per check and make the linear lookups quadratic overall.
+	if (is_type_polymorphic(named_type)) {
+		return;
+	}
+
 	auto *found_gen_types = ensure_polymorphic_record_entity_has_gen_types(ctx, original_type);
 	mutex_lock(&found_gen_types->mutex);
 	defer (mutex_unlock(&found_gen_types->mutex));
